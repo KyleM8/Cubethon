53779ef59bc080f4c7dacf4d59821b66e6d70a1d
KyleM8 <kylejm220@gmail.com>
2020-10-20 16:43:28 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody playerRigidbody;
+    private int playerSpeed = 2000;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement() {
+        playerRigidbody.AddForce(0, 0, playerSpeed * Time.deltaTime);
+    }
+
+
+}
