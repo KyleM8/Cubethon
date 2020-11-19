1d43e273417cf15e7d9f8cc9c6db54b9afccaa13
Kyle Markel <kylejm220@gmail.com>
2020-11-19 09:12:17 -0700

    changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private InputManager inputManager;
     [SerializeField] private Rigidbody playerRigidbody;
-    [SerializeField] private float forwardForce = 5f;
-    [SerializeField] private float playerForce = 3f;
+    [SerializeField] private float forwardForce = 0.001f;
+    [SerializeField] private float playerForce = 0.0005f;
     
 
     private void Awake() {
         inputManager = InputManager.Instance;
+        playerRigidbody.position = new Vector3(0f,0f,0f);
     }
 
     void FixedUpdate() {
@@ -19,14 +20,14 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Movement() {
-        playerRigidbody.AddForce(0, 0, forwardForce * Time.deltaTime);
+        playerRigidbody.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Acceleration);
         Vector2 playerInput = inputManager.GetPlayerMovement();
         // Debug.Log("input: " + playerInput.x);
         if (playerInput.x == -1) {
-            playerRigidbody.AddForce(-playerForce * Time.deltaTime, 0, 0);
+            playerRigidbody.AddForce(-playerForce * Time.deltaTime, 0, 0, ForceMode.Acceleration);
         }
         else if (playerInput.x == 1) {
-            playerRigidbody.AddForce(playerForce * Time.deltaTime, 0, 0);
+            playerRigidbody.AddForce(playerForce * Time.deltaTime, 0, 0, ForceMode.Acceleration);
         }
     }
 
